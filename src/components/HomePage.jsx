df5880b8e7bd50fa4e1950a7b47983b294ab491c
Kubra Tokgozlu <kubra.tokgozlu@gmail.com>
2023-12-13 14:30:06 +0100

    comment lines deleted

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,39 +1,3 @@
-/*import React, { useState } from 'react';
-import productsData from '../assets/data/products.json'; 
-
-const Home = () => {
-  const [products, setProducts] = useState(productsData);
-
-  const handleDelete = (id) => {
-    const updatedProducts = products.filter((product) => product.id !== id);
-    setProducts(updatedProducts);
-  };
-
-  return (
-    <div>
-      <h2>Home Page!</h2>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <div>
-              <h3>{product.title}</h3>
-              <p>{product.description}</p>
-              <p>Price: ${product.price}</p>
-              {product.discountPercentage > 0 ? (
-                <p>Discounted Price: ${product.price - (product.price * product.discountPercentage) / 100}</p>
-              ) : null}
-              <button onClick={() => handleDelete(product.id)}>Delete</button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default Home;*/
-
-
 import React, { useState } from 'react';
 import ProductList from '../components/ProductList';
 import productsData from '../assets/data/products.json';
